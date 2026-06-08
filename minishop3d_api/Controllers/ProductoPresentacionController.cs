547decccf222f72093b72e15f111f2fae5839fb4
DanielRodJ <106783688+DanielRodJ@users.noreply.github.com>
2026-06-08 11:29:18 -0600

    feat: add producto presentacion create, update, and calculate endpoints

---
+++ minishop3d_api/Controllers/ProductoPresentacionController.cs
@@ -0,0 +1,57 @@
+﻿using Application.Features.ProductoPresentaciones.Commands.AddProductoPresentacion;
+using Application.Features.ProductoPresentaciones.Commands.CalculateProductoPresentacion;
+using Application.Features.ProductoPresentaciones.Commands.UpdateProductoPresentacion;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace minishop3d_api.Controllers
+{
+
+    [ApiController]
+    [Route("minisho3d/producto-presentacion")]
+    public class ProductoPresentacionController : BaseController
+    {
+        private readonly ISender _sender;
+
+        public ProductoPresentacionController(ISender sender)
+        {
+            _sender = sender ?? throw new ArgumentNullException(nameof(sender));
+        }
+
+        [Authorize]
+        [HttpPost]
+        public async Task<IActionResult> AddProductoPresentacionAsync(
+            [FromBody] AddProductoPresentacionCommand command,
+            CancellationToken cancellationToken)
+        {
+            var result = await _sender.Send(command, cancellationToken);
+            return HandleResult(result);
+        }
+
+        [Authorize]
+        [HttpPut("{productoPresentacionId:int:required}")]
+        public async Task<IActionResult> UpdateProductoPresentacionAsync(
+            [FromRoute] int productoPresentacionId,
+            [FromBody] UpdateProductoPresentacionCommand command,
+            CancellationToken cancellationToken)
+        {
+            if(productoPresentacionId != command.ProductoPresentacionId)
+            {
+                command.ProductoPresentacionId = productoPresentacionId;
+            }
+
+            var result = await _sender.Send(command, cancellationToken);
+            return HandleResult(result);
+        }
+
+        [HttpPost("calculate-presentacion")]
+        public async Task<IActionResult> CalculateProductoPresentacionAsync(
+            [FromBody] CalculateProductoPresentacionCommand command,
+            CancellationToken cancellationToken)
+        {
+            var result = await _sender.Send(command, cancellationToken);
+            return HandleResult(result);
+        }
+    }
+}
